02fbad9ec87ab9784f3f52dbae13a84741c1f192
Eduardo8895 <eduardo.martins.du1509@gmail.com>
2026-09-09 22:41:33 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+function App(){
+    return(
+        <div className="bg-ligth min-vvh-100 py-4">
+            <div className="container">
+               <header className="row mb-4">
+                <div className="col text-center">
+                    <h1 className="display-5 fw-bold text-primary">
+                        Projeto React + Bootstrap Grid
+                    </h1>
+                    <p className="lead text-scondary">
+                        exemplo de layout responsivo utilizando o sistema de colunas do Bootstrap
+                    </p>
+                </div>
+               </header>
+            </div>
+        </div>
+    );
+}
+
+export default App;
